03867a0648011f49d2022f5e605afc704bfbedff
Evilless63 <vitaliy030589@gmail.com>
2020-03-29 16:48:39 +0400

    ff

--- mailto.php
+++ mailto.php
@@ -4,29 +4,30 @@ include_once('PHPMailer/Exception.php');
 include_once('PHPMailer/PHPMailer.php'); 
 include_once('PHPMailer/SMTP.php');
 
-        $key = hash('sha256', $_POST['LMI_PAYEE_PURSE'] .
-            $_POST['LMI_PAYMENT_AMOUNT'] .
-            $_POST['LMI_PAYMENT_NO'] .
-            $_POST['LMI_MODE'] .
-            $_POST['LMI_SYS_INVS_NO'] .
-            $_POST['LMI_SYS_TRANS_NO'] .
-            $_POST['LMI_SYS_TRANS_DATE'] .
-            '12345678' .
-            $_POST['LMI_PAYER_PURSE'] .
-            $_POST['LMI_PAYER_WM']);
+//         $key = hash('sha256', $_POST['LMI_PAYEE_PURSE'] .
+//             $_POST['LMI_PAYMENT_AMOUNT'] .
+//             $_POST['LMI_PAYMENT_NO'] .
+//             $_POST['LMI_MODE'] .
+//             $_POST['LMI_SYS_INVS_NO'] .
+//             $_POST['LMI_SYS_TRANS_NO'] .
+//             $_POST['LMI_SYS_TRANS_DATE'] .
+//             '12345678' .
+//             $_POST['LMI_PAYER_PURSE'] .
+//             $_POST['LMI_PAYER_WM']);
 
-        if (strtoupper($key) != $_POST['LMI_HASH']) {
-exit();
-		} else {
+//         if (strtoupper($key) != $_POST['LMI_HASH']) {
+// exit();
+// 		} else {
             $email = new PHPMailer();
             $email->SetFrom('vladislava@gmail.com', 'Поэт'); //Name is optional
             $email->Subject   = 'Стихи';
             $email->Body      = 'Пароль от архива: yreyuifuhwhfiuhiu837438';
-            $email->AddAddress( $_POST['paymentemail'] );
+            // $email->AddAddress( $_POST['paymentemail'] );
+            $email->AddAddress( 'vitaliy030589@gmail.com' );
             
             $file_to_attach = 'stihi.zip';
             
             $email->AddAttachment( $file_to_attach , 'stihi.zip' );
             
             return $email->Send();
-	} 
+	// } 
